60e842ad110ff527c43d0cc42db0c6b5a6c28558
Leo Davis <ldav1s@yahoo.com>
2015-06-28 21:49:05 -0600

    Added fixes.
    shellcheck.net flagged these issues.

--- customization/bin/hooks/hook_piratebox_start_done.sh
+++ customization/bin/hooks/hook_piratebox_start_done.sh
@@ -3,15 +3,15 @@
 # ---- TEMPLATE ----
 
 # Runs on every Startup after the normal init-steps are done
-#  get config file 
+#  get config file
 
-if [ !  -f $1 ] ; then
+if [ !  -f "$1" ] ; then
   echo "Config-File $1 not found..."
   exit 255
 fi
 
 #Load config
-. $1
+. "$1"
 
 # You can uncommend this line to see when hook is starting:
  echo "------------------ Running $0 ------------------"
@@ -31,7 +31,7 @@ if [ "$FTP_ENABLED" = "yes" ] ; then
 	echo "starting PROFTPD.."
 
 	# Load PirateBox config
-	. $PIRATEBOX_FOLDER/conf/ftp/ftp.conf
+	. "$PIRATEBOX_FOLDER/conf/ftp/ftp.conf"
 
 	# $PROFTPD_CONFIG_FILE
 	# $PROFTPD_PID  #####PID#####
@@ -48,9 +48,9 @@ if [ "$FTP_ENABLED" = "yes" ] ; then
 
 	if [ "$ADMIN_ACCESS" = "no" ] &&  [ "$ENABLE_ANON"  = "no" ] ; then
 		echo "skip ftp, because admin and anon disabled"
-	else 
+	else
 		#Proftpd writes the pidfile for its own
-		proftpd  -c $PROFTPD_CONFIG_FILE $proftpd_opt_admin $proftpd_opt_admin $proftpd_opt_sync 
+		proftpd  -c "$PROFTPD_CONFIG_FILE" $proftpd_opt_admin $proftpd_opt_anon
 		echo $?
 	fi
 
@@ -58,25 +58,25 @@ fi
 
 
 if [ "$FTP_SYNC_ENABLED" = "yes" ] ; then
-	echo "start PROFTPD with Sync config ..." 
+	echo "start PROFTPD with Sync config ..."
 	#Proftpd writes the pidfile for its own
-	proftpd  -c $PROFTPD_SYNC_CONFIG_FILE 
+	proftpd  -c "$PROFTPD_SYNC_CONFIG_FILE"
 	echo $?
 fi
 
 
 
-if [ "$SHOUTBOX_ENABLED" == "no" ] ; then
+if [ "$SHOUTBOX_ENABLED" = "no" ] ; then
 	# If the shoutbox is disabled, we remove the writable flag
-	echo -n "Making shoutbox readonly..."
-	chmod a-w $CHATFILE
+	echo "Making shoutbox readonly...\c"
+	chmod a-w "$CHATFILE"
 	echo "done"
 fi
 
- $PIRATEBOX_FOLDER/bin/json_generation.sh  $1
+ "$PIRATEBOX_FOLDER/bin/json_generation.sh"  "$1"
 
-if [ "$FTP_SYNC_CLIENT_ENABLED" == "yes" ] ; then
+if [ "$FTP_SYNC_CLIENT_ENABLED" = "yes" ] ; then
 	echo "Starting sync client"
-	$PIRATEBOX_FOLDER/bin/ftp_sync_tool.sh >>  $PIRATEBOX_FOLDER/share/sync.log & 
-	echo $! > $FTP_SYNC_CLIENT_PID
+	"$PIRATEBOX_FOLDER/bin/ftp_sync_tool.sh" >>  "$PIRATEBOX_FOLDER/share/sync.log" &
+	echo $! > "$FTP_SYNC_CLIENT_PID"
 fi
